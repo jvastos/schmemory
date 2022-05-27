ea05a1371873d71e17b8cec218adacb103298a10
João Almeida <jvastos@gmail.com>
2022-05-27 23:15:56 +0200

    prettified

--- src/scripts/modules/timer.js
+++ src/scripts/modules/timer.js
@@ -1,30 +1,30 @@
 const element = {
-    minutes: document.getElementById("minutes"),
-    seconds: document.getElementById("seconds"),
-}
+  minutes: document.getElementById('minutes'),
+  seconds: document.getElementById('seconds'),
+};
 
 //Variables to capture minutes and seconds to be used in the timer.
 export let minutes = 0;
 export let seconds = 0;
 
-element.minutes.innerHTML = minutes
-element.seconds.innerHTML = seconds
+element.minutes.innerHTML = minutes;
+element.seconds.innerHTML = seconds;
 
 //Interval to count minutes.
 export const minutesInterval = setInterval(() => {
-    minutes++;
-    element.minutes.innerHTML = minutes
-    seconds = 0;
+  minutes++;
+  element.minutes.innerHTML = minutes;
+  seconds = 0;
 }, 60000);
 
 //Interval to count seconds.
 export const secondsInterval = setInterval(() => {
-    seconds++;
-    element.seconds.innerHTML = seconds
+  seconds++;
+  element.seconds.innerHTML = seconds;
 }, 1000);
 
 //Function to get both intervals started at the same time.
 export function startTimer() {
-    minutesInterval;
-    secondsInterval;
+  minutesInterval;
+  secondsInterval;
 }
